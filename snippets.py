e45bd9d56e03ac3e9e00ccfde0f7e426b166e9d5
Santiago Diaz <zantiago29@hotmail.com>
2017-05-10 23:47:12 +0000

    adding parser

--- snippets.py
+++ snippets.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 #set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -21,6 +22,43 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructor parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+    
+    #Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Retrieve the snippet")
+    get_parser.add_argument("name", help="Name of the snippet")
+    
+    arguments = parser.parse_args()
+    
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+    
+if __name__ == "__main__":
+    main()
+    
+
+    
     
     
     
